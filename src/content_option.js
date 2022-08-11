68c3b58bd39277d20935725feafbb14d46e37504
olorunwa lawrence <olorunwalaw@gmail.com>
2022-08-10 17:26:39 -0700

    create a portfolio website

---
+++ src/content_option.js
@@ -0,0 +1,154 @@
+const logotext = "Olorunwa";
+const meta = {
+  title: "Olorunwa Lawrence",
+  description:
+    "I’m Olorunwa Lawrence front end devloper,currently working in as a freelancer",
+};
+
+const introdata = {
+  title: "I’m Olorunwa Lawrence",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third: "I develop web with reactjs",
+  },
+  description:
+    "One of my greatest desire is to see your business growing by taing advantage of web technology",
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+  title: "Abit about my self",
+  aboutme:
+    "2+ years experienced React Developer with hands on experience in identifying web-based user interactions along with designning and implementing highly responsive user interface components by deploying react concepts, proficient in translating designs and wireframes into high-quality code, and writing apllication interface code via javascript and reactjs workflow. adept at monitoring and maintaing frontend performance and troubleshooting and debugging the same to boister over performance.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Designer of week",
+    where: "YAdfi",
+    date: "2020",
+  },
+  {
+    jobtitle: "Designer of week",
+    where: "Jamalya",
+    date: "2019",
+  },
+  {
+    jobtitle: "Designer of week",
+    where: "ALquds",
+    date: "2019",
+  },
+];
+
+const skills = [
+  {
+    name: "Javascript",
+    value: 90,
+  },
+  {
+    name: "ReactJs",
+    value: 85,
+  },
+  {
+    name: "Git and Github",
+    value: 80,
+  },
+  {
+    name: "Redux",
+    value: 85,
+  },
+  {
+    name: "Styled-Components",
+    value: 85,
+  },
+  {
+    name: "tail-wind css",
+    value: 75,
+  },
+  {
+    name: "Material-Ui",
+    value: 95,
+  },
+  {
+    name: "Ant-Design",
+    value: 80,
+  },
+  {
+    name: "Css",
+    value: 90,
+  },
+];
+
+const services = [
+  {
+    title: "UI Designer",
+    // description:
+    //   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "Web Dveloper",
+    // description:
+    //   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "Shopifies is a clone of an eccomerce website with basic functionality and fullu responsive",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    desctiption:
+      ".... is a website that display information about various crypo currency from two diffeerent apis",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "this is a movie app the allows users to search for movies and  display informations about each movies searched for, and its fully responsive",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "olorunwalawrence5@gmail.com",
+  YOUR_FONE: "+2347015009775",
+  description:
+    "Kindly send me a message and i will respond to you as soon as possible.",
+  // creat an emailjs.com account
+  // check out this tutorial https://ww w.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "contact_service",
+  YOUR_TEMPLATE_ID: "contact_form",
+  YOUR_USER_ID: "P5A--XupAcWfW1-he",
+};
+
+const socialprofils = {
+  github: "https://github.com",
+  facebook: "https://facebook.com",
+  linkedin: "https://linkedin.com",
+  //   twitter: "https://twitter.com",
+  //   youtube: "https://youtube.com",
+  //   twitch: "https://twitch.com",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
